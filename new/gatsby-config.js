7f30d595c5ae9c2a2337d7813b68e19d9ae3a10b
phatpham9 <phatpham9@gmail.com>
2019-12-16 11:40:49 +0700

    build: remove gatsby-plugin-feed, add prefers-color-scheme detect, add host 0.0.0.0 to npm scripts

--- new/gatsby-config.js
+++ new/gatsby-config.js
@@ -39,55 +39,6 @@ module.exports = {
       options: {},
     },
     {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title: siteTitle
-                description: siteDescription
-                siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allPost } }) => {
-              return allPost.nodes.map(post => {
-                return {
-                  title: post.title,
-                  date: post.date,
-                  excerpt: post.excerpt,
-                  url: site.siteMetadata.siteUrl + post.slug,
-                  guid: site.siteMetadata.siteUrl + post.slug,
-                  custom_elements: [{
-                    'content:encoded': post.html
-                  }],
-                }
-              })
-            },
-            query: `
-              {
-                allPost(sort: { fields: date, order: DESC }) {
-                  nodes {
-                    title
-                    date(formatString: "MMMM D, YYYY")
-                    excerpt
-                    slug
-                    html
-                  }
-                }
-              }
-            `,
-            output: 'rss.xml',
-            title: 'Phat Pham | OnRoads.xyz',
-          },
-        ],
-      },
-    },
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Phat Pham | OnRoads.xyz',
