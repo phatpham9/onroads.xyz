3fd2b93da0f625dd3fa37409c6fb58c88487ecae
phatpham9 <phatpham9@gmail.com>
2021-03-27 17:02:03 +0700

    feat(theme): shadow footer component, add source code's github repo link, relayout on mobile

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,41 @@
+/** @jsx jsx */
+import { jsx, Link } from 'theme-ui';
+import useSiteMetadata from '@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata';
+
+const Footer = () => {
+  const { siteTitle } = useSiteMetadata();
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [4],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy;{new Date().getFullYear()} by {siteTitle}
+      </div>
+      <div>
+        Code
+        {` `}
+        on
+        {` `}
+        <Link
+          aria-label="Link to the website's GitHub repository"
+          href="https://github.com/phatpham9/onroads.xyz"
+        >
+          GitHub
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
